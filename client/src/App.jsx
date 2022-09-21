ca41eef5c2ed43b6e51754e8b0da2ba380b08913
Marielle <marielle.ibias@example.com>
2022-09-21 10:29:48 +0200

    #2 #3 fixing login and register routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,7 @@
 import { ThemeProvider } from '@mui/system';
 import theme from "./styles/theme";
-import { Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/useAuth";
-import { ProtectedRoute } from "./components/routes/ProtectedRoute";
-import { Home } from "./pages/Home";
-import { Login } from "./pages/Login";
-import { Register } from "./pages/Register";
-import Footer from "./components/footer";
+import AppRoutes from './components/routes/AppRoutes';
 
 export default function App() {
 
@@ -14,20 +9,9 @@ export default function App() {
 
     <ThemeProvider theme={theme}>
       <AuthProvider>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <ProtectedRoute>
-                <Register />
-                <Home />
-                <Footer />
-              </ProtectedRoute>
-            }
-          />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
+        <AppRoutes>
+
+        </AppRoutes>
       </AuthProvider>
     </ThemeProvider>
   );
--- client/src/components/routes/AppRoutes.jsx
+++ client/src/components/routes/AppRoutes.jsx
@@ -2,12 +2,23 @@ import { Routes, Route } from "react-router-dom";
 import { ProtectedRoute } from "./ProtectedRoute";
 import { Home } from "../../pages/Home";
 import { Login } from "../../pages/Login";
+import { Register } from "../../pages/Register";
 
 export default function AppRoutes() {
     return (
-            <Routes>
-                <Route path="/" element={<ProtectedRoute> <Home /> </ProtectedRoute>}/>
-                <Route path="/login" element={<Login />} />
-            </Routes>
+        <Routes>
+            <Route path="/login" element=
+                {<Login />}
+            />
+            <Route path="/register" element=
+                {<ProtectedRoute>
+                    <Register />
+                </ProtectedRoute>}
+            />
+            <Route path="/" element={<ProtectedRoute>
+                <Home />
+            </ProtectedRoute>}
+            />
+        </Routes>
     )
 }
