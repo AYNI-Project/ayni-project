a4ecf49b56ba9c8dd3843facfdf3494a8f2d2b89
chechu <ceciaolmedo@gmail.com>
2022-09-27 14:56:57 +0200

    server setup

---
+++ server2/src/app.ts
@@ -0,0 +1,11 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(cors());
+
+
+app.listen(PORT, ()=> console.log(`listening on ${PORT}`));
