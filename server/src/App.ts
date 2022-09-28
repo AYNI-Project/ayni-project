3ecc73d57d232c8c300e17fbc87cef74885c6cfd
chechu <ceciaolmedo@gmail.com>
2022-09-28 14:51:38 +0200

    #5 Card details

--- server/src/App.ts
+++ server/src/App.ts
@@ -1,9 +1,13 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+//import { router } from "./routes";
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+
+//app.use(router);
 
 app.use(cors());
 
