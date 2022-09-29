8fd67e16d9b9cea9a5d8b7324a7e659b83be5585
Marielle <marielle.ibias@example.com>
2022-09-29 17:05:13 +0200

    revert to 280922 version

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { ThemeProvider } from '@mui/system';
 import theme from "./styles/theme";
 import { AuthProvider } from "./context/useAuth";
 import AppRoutes from './components/routes/AppRoutes';
-
+import "./App.css";
 
 
 export default function App() {
--- client/src/components/appbar/AppbarMobile.js
+++ client/src/components/appbar/AppbarMobile.js
@@ -1,23 +1,30 @@
 import { IconButton } from "@mui/material";
-import { AppbarContainer, AppbarHeader, LogoImage } from "../../styles/appbar";
+import { AppbarContainer, LogoImage } from "../../styles/appbar";
 import MenuIcon from "@mui/icons-material/Menu";
 import SearchIcon from "@mui/icons-material/Search";
 import Actions from "./Actions";
+import { useNavigate } from "react-router-dom";
 
 export default function AppbarMobile({ matches }) {
-    return (
-        <AppbarContainer> 
-            <IconButton>
-                <MenuIcon />
-            </IconButton>
-            <LogoImage src="./images/banner/2.png" alt="logo-Ayni" />
-            {/*<AppbarHeader textAlign={"center"} variant="h4">
-                Ayni
-            </AppbarHeader>*/}
-            <IconButton>
-                <SearchIcon />
-            </IconButton>
-            <Actions matches={matches}  />
-        </AppbarContainer>
-    )
+  const navigate = useNavigate();
+
+  const navigateToHome = () => {
+    navigate("/");
+  };
+  return (
+    <AppbarContainer>
+      <IconButton>
+        <MenuIcon />
+      </IconButton>
+      <LogoImage
+        src="./images/banner/2.png"
+        alt="logo-Ayni"
+        onClick={navigateToHome}
+      />
+      <IconButton>
+        <SearchIcon />
+      </IconButton>
+      <Actions matches={matches} />
+    </AppbarContainer>
+  );
 }
