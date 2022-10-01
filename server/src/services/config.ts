631a04adfc0dc1dac6677f29fd2bdb0b4b91a958
Maria <maria.carranzavaldes@gmail.com>
2022-10-01 18:46:06 +0200

    #1, seting config.ts with new uri

--- server/src/services/config.ts
+++ server/src/services/config.ts
@@ -3,7 +3,7 @@ import { config as dotenvConfig } from "dotenv";
 dotenvConfig();
 
 export const config = (): any => {
-  const uri = process.env.POSTGRES_URL as string;
+  const uri = process.env.HEROKU_POSTGRESQL_YELLOW_URL as string;
   return uri;
 };
 export const secret = process.env.SECRET || "";
