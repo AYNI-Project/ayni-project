0e1eda5763bd06d2a9863d45a407b08df83bd68f
Maria <maria.carranzavaldes@gmail.com>
2022-10-03 10:53:16 +0200

    #9, axios.put

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -4,51 +4,55 @@ import PhotoCamera from "@mui/icons-material/PhotoCamera";
 import IconButton from "@mui/material/IconButton";
 import Appbar from "../components/appbar";
 import { InputForm, LeftContent, RightContent, Subtitle, View } from "../styles/auth";
-import { useState, setState } from "react";
+import { useState} from "react";
+import axios from 'axios'
 
 export default function Profile() {
-
+  const [user, setUser] = useState([]);
   const [name, setName] = useState('');
   const [lastName, setLastName] = useState('');
   const [city, setCity] = useState('');
+  const [celNumber, setCelNumber] = useState('');
   const [email, setEmail] = useState('');
   const [password,setPassword] = useState('');
   const [confirmPassword,setConfirmPassword] = useState('');
   const [aboutMe, setAboutMe] = useState('');
   const [picture, setPicture] = useState('');
   
-  const handleInputChange = (e) => {
-    const {id , value} = e.target;
-    if(id === "name"){
-        setName(value);
-    }
-    if(id === "lastName"){
-        setLastName(value);
-    }
-    if(id === "city"){
-      setCity(value);
-    }
-     if(id === "email"){
-        setEmail(value);
-    }
-    if(id === "password"){
-        setPassword(value);
-    }
-    if(id === "confirmPassword"){
-        setConfirmPassword(value);
-    }
-    if(id === "aboutMe"){
-      setAboutMe(value);
-    }
-    if(id === "picture"){
-      setPicture(value);
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+       .put('https://ayni-project.herokuapp.com/user/edit/:id', {
+          name: name,
+          lastName: lastName,
+          city: city,
+          celNumber: celNumber,
+          email: email,
+          password: password,
+          confirmPassword: confirmPassword,
+          aboutMe: aboutMe,
+          picture: picture
 
-}
 
-const handleSubmit  = () => {
-    console.log(name,lastName,city,email,password,confirmPassword,aboutMe,picture);
-}
+       })
+       .then((res) => {
+          setUser((user) => [res.data, ...user]);
+          setName('');
+          setLastName('');
+          setCity('');
+          setCelNumber('');
+          setEmail('');
+          setPassword ('');
+          setConfirmPassword ('');
+          setAboutMe ('');
+          setPicture('')
+       })
+       .catch((err) => {
+          console.log(err.message);
+       });
+ }; 
+
+
   return (
     <>
       <Appbar />
@@ -62,7 +66,7 @@ const handleSubmit  = () => {
             label="Nombre"
             variant="standard"
             value={name}
-            onChange = {(e) => handleInputChange(e)}
+            onChange={(e)=> setName(e.target.value)}
           />
           <InputForm
             type={"text"}
@@ -70,7 +74,7 @@ const handleSubmit  = () => {
             label="Apellidos"
             variant="standard"
             value={lastName}
-            onChange = {(e) => handleInputChange(e)}
+            onChange={(e)=> setLastName(e.target.value)}
           />
           <InputForm
             type={"text"}
@@ -78,7 +82,15 @@ const handleSubmit  = () => {
             label="Ciudad"
             variant="standard"
             value={city}
-            onChange = {(e) => handleInputChange(e)}
+            onChange={(e)=> setCity(e.target.value)}
+          />
+          <InputForm
+            type={"text"}
+            id="celNumber "
+            label="Teléfono Móvil"
+            variant="standard"
+            value={celNumber}
+            onChange = {(e) => setCelNumber(e.target.value)}
           />
           <InputForm
             type={"email"}
@@ -86,7 +98,7 @@ const handleSubmit  = () => {
             label="Email"
             variant="standard"
             value={email}
-            onChange = {(e) => handleInputChange(e)}
+            onChange={(e)=> setEmail(e.target.value)}
           />
           <InputForm
             type="password"
@@ -94,15 +106,15 @@ const handleSubmit  = () => {
             label="Contraseña"
             variant="standard"
             value={password}
-            onChange = {(e) => handleInputChange(e)}/>
-          
+            onChange={(e)=> setPassword(e.target.value)}
+          />
           <InputForm
             type="password"
             id="confirmPassword"
             label="Confirmar Contraseña"
             variant="standard"
             value={confirmPassword}
-            onChange = {(e) => handleInputChange(e)}
+            onChange={(e)=> setConfirmPassword(e.target.value)}
           />
         </LeftContent>
         <RightContent>
@@ -113,7 +125,7 @@ const handleSubmit  = () => {
             rows={3}
             variant="standard"
             value={aboutMe}
-            onChange = {(e) => handleInputChange(e)}
+            onChange={(e)=> setAboutMe(e.target.value)}
           />
            <IconButton
             color="inherit"
@@ -122,19 +134,50 @@ const handleSubmit  = () => {
             variant= 'text'
             style={{padding: "2rem"}}
             value={picture}
-            onChange = {(e) => handleInputChange(e)}
+            onChange={(e)=> setPicture(e.target.value)}
           >
             <Typography style={{ color: "#696969",paddingRight: "2rem" }}>Elige una foto de perfil</Typography>
             <input hidden accept="image/*" type="file" />
             <PhotoCamera />
           </IconButton>         
           <Box style={{ padding: "2rem" }}>
-            <Button className="btn" type="submit" variant="outlined" color='inherit' style={{backgroundColor:"#FFA37F"}} onClick={()=>handleSubmit()}>
+            <Button className="btn" type="submit" variant="outlined" color='inherit' style={{backgroundColor:"#FFA37F"}} onClick={handleSubmit}>
               Guardar perfil
             </Button>
           </Box>
         </RightContent>
       </View>
-    </>
+          </>
   );
 }
+
+/*onClick={()=>handleSubmit()}
+
+const handleInputChange = (e) => {
+  const {id , value} = e.target;
+  if(id === "name"){
+      setName(value);
+  }
+  if(id === "lastName"){
+      setLastName(value);
+  }
+  if(id === "city"){
+    setCity(value);
+  }
+   if(id === "email"){
+      setEmail(value);
+  }
+  if(id === "password"){
+      setPassword(value);
+  }
+  if(id === "confirmPassword"){
+      setConfirmPassword(value);
+  }
+  if(id === "aboutMe"){
+    setAboutMe(value);
+  }
+  if(id === "picture"){
+    setPicture(value);
+  }
+
+}*/
